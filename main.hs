747d7eaa86f21f3fa26e1bd97d23afa5a0d52f05
cristian Gonzalez <you@example.com>
2022-01-07 00:01:14 -0600

    init

---
+++ main.hs
@@ -0,0 +1,33 @@
+type Person = String
+
+type Book = String
+
+type Database = [(Person, Book)]
+
+exampleBase :: Database
+exampleBase =
+  [ ("Alice", "TinTin"),
+    ("Anna", "Little Women"),
+    ("Alice", "Asterix"),
+    ("Rory", "TinTin")
+  ]
+
+books :: Database -> Person -> [Book]
+books dBase findPerson =
+  [book | (person, book) <- dBase, person == findPerson]
+
+-- borrowers :: Database -> Book -> [Person]
+borrowed :: Database -> Book -> [Bool]
+borrowed dBase bo = [ book| (per, book) <- dBase,book == bo]
+
+-- numBorrowed :: Database -> Person -> Int
+makeLoan :: Database -> Person -> Book -> Database
+makeLoan dBase person book =
+  [(person, book)] ++ dBase
+
+returnLoan :: Database -> Person -> Book -> Database
+returnLoan dBase person book =
+  [pair | pair <- dBase, pair /= (person, book)]
+
+main :: IO ()
+main = print $ borrowed exampleBase "Little Women"
